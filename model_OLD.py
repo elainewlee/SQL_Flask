0b9ac438a80b73fc9ad4ce9b686d7f9172737eeb
Hackbright Student <user@mini-006.(none)>
2013-03-29 12:43:50 -0700

    Part 1 complete

--- model_OLD.py
+++ model_OLD.py
@@ -1,47 +1,51 @@
 import sqlite3
 import datetime
 
+
+DB = None
+CONN = None
+
 def connect_to_db():
-	return sqlite3.connect("tipsy.db")
+	global DB, CONN
+	CONN = sqlite3.connect("tipsy.db")
+	DB = CONN.cursor()
 	 
-def new_user(db, email, password, name):
-	c = db.cursor()
+
+def new_user(DB, id, email, password, name):
+	c = DB.cursor()
 	query = """INSERT INTO Users VALUES	(NULL, ?,?,?)"""
-	result = c.execute(query, (email, password, name))
-	db.commit()
+	result = DB.execute(query, (id, email, password, name))
+	CONN.commit()
 	return result.lastrowid
 
 #given a username and password, returns a dictionary of a user's fields pulled from the database, and a None if the credentials do not match
-def authenticate(db, email, password):
-	c = db.cursor()
-	query = """SELECT * FROM Users WHERE email=? AND password=?"""
-	c.execute(query, (email, password))
-	result = c.fetchone() #return ONE single from Users table matching row to the query
-	db.commit()
+def authenticate(DB, name, password):
+	query = """SELECT * FROM Users WHERE name=? AND password=?"""
+	DB.execute(query, (name, password))
+	result = DB.fetchone() #return ONE single from Users table matching row to the query
+	CONN.commit()
 	#Create a dictionary
 	if result:
-		#fields = ["email", "password"]
-		fields = ["id", "email", "password", "email"] #This is the field/column email of the dictionary
+		#fields = ["name", "password"]
+		fields = ["id", "email", "password", "name"] #This is the field/column name of the dictionary
 		return dict(zip(fields, result)) #This returns a dictionary created from the zip pairing of the 4 text names in fields with the 4 values in result, which came fron the 4 columns in the Users table.
 	else:
-		return None #Else no email and password given, resulting in None
+		return None #Else no username and password given, resulting in None
 
 #Creates a new task, returns the id of theturn the user as a dictionary, like our authenticate method. newly created row. Make sure to populate the created_at field.
-def new_task(db, title, user_id): #(db, id, title, created_at, completed_at, user_id):
-	c = db.cursor()
+def new_task(DB, title, user_id): #(DB, id, title, created_at, completed_at, user_id):
 	query = """INSERT INTO Tasks VALUES (NULL, ?,?, NULL, ?)"""#(id, title, created_at, completed_at, user_id)"""
 	t = datetime.datetime.utcnow() # t = created_at
-	result = c.execute(query, (title, t, user_id))
-	db.commit()
+	result = DB.execute(query, (title, t, user_id))
+	CONN.commit()
 	return result.lastrowid
 
 
-def get_user(db, id): #Fetch a user's record based on his id. Return the user as a dictionary, like our authenticate method.
-	c = db.cursor()
+def get_user(DB, id): #Fetch a user's record based on his id. Return the user as a dictionary, like our authenticate method.
 	query = """SELECT * FROM Users WHERE id=?"""
-	c.execute(query,(id))
-	result = c.fetchone()
-	db.commit()
+	DB.execute(query,(id))
+	result = DB.fetchone()
+	CONN.commit()
 	#Create a dictionary
 	if result:
 		fields = ["id", "email", "password", "name"] #Fields is the column name of the dictionary
@@ -49,40 +53,39 @@ def get_user(db, id): #Fetch a user's record based on his id. Return the user as
 	else:
 		return None #Else no username and password given, resulting in None
 
-def complete_task(db, id): #Marks a task as being complete, setting the completed_at field.
-	c = db.cursor()
+def complete_task(DB, id): #Marks a task as being complete, setting the completed_at field.
 	completed_at = datetime.datetime.utcnow() 
 	query = """UPDATE Tasks SET completed_at = ? WHERE id=?""" # t = completed_at
-	result = c.execute(query,(completed_at, id))
-	db.commit()
+	result = DB.execute(query,(completed_at, id))
+
+	CONN.commit()
 	return id	
 
-def get_tasks(db, user_id=None): #Gets all the tasks for the given user id. Returns all the tasks in the system if no user_id is given. Returns them as a list of dictionaries.
-	c = db.cursor()
+def get_tasks(DB, user_id=None): #Gets all the tasks for the given user id. Returns all the tasks in the system if no user_id is given. Returns them as a list of dictionaries.
+	
 	if user_id == None: #Returns all the tasks in the system if no user_id is given.
 		query = """SELECT * FROM Tasks"""
-		cursor = c.execute(query)
+		cursor = DB.execute(query)
 		rows = cursor.fetchall()		
-		#return rows
+		return rows
 	else: #Gets all the tasks for the given user id.Returns them as a list of dictionaries.
 		query = """SELECT * FROM Tasks WHERE user_id=?"""
-		cursor = c.execute(query,(user_id, )) # Tuple of
+		cursor = DB.execute(query,(user_id, )) # Tuple of
 		rows = cursor.fetchall()	
-		#return rows
+		return rows
 
 	tasks = []
 	for row in rows: #add individual tasks to task list
 		task = dict(zip(["id","title", "created_at", "completed_at", "user_id"], row))
-		tasks.append(task)
+		tasks.append(tasks)
 	return tasks
 
 
-def get_task(db, id): #Get a single task, given its id. Return the task as a dictionary as above in the authenticate method.
-	c = db.cursor()
+def get_task(DB, id): #Get a single task, given its id. Return the task as a dictionary as above in the authenticate method.
 	query = """SELECT * FROM Tasks WHERE id=?"""
-	c.execute(query,(id))
-	result = c.fetchone()
-	db.commit()
+	DB.execute(query,(id))
+	result = DB.fetchone()
+	CONN.commit()
 	#Create a dictionary
 	if result:
 		fields = ["id", "title", "created_at", "completed_at", "user_id"] #Fields is the column name of the dictionary
--- tipsy.py
+++ tipsy.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import model
+
 
 # Initialize program to be a Flask application
 # Flask is a class
@@ -11,16 +12,26 @@ app = Flask(__name__)
 # Return value sent to browser
 @app.route("/") #'decorator' that tells Flask what url (route) is attached to this function
 def index():
-	return render_template("index.html", user_name="lani")
+	return render_template("index.html", name="Lani")
 
 @app.route("/tasks")
 def list_tasks():
-	db = model.connect_to_db()
-	tasks_from_db = model.get_tasks(DB, None)
-	return render_template("list_tasks.html", tasks= tasks_from_db)
+	db = model.connect_to_db() # connect to the database
+	tasks_from_db = model.get_tasks(db, None) # get a list of all the tasks
+	return render_template("list_tasks.html", tasks= tasks_from_db) #send that list to the list_tasks template as a parameter named "tasks"
 
+@app.route("/new_task")
+def new_tasks():
+	return render_template("new_task.html")
+
+@app.route("/save_task", methods=["POST"]) #This url will respond to POSTed forms, rather than plain url requests.
+def save_task():
+	task_title = request.form['task_title']
+	db = model.connect_to_db()
+	#Assume that all tasks are attached to user 1.
+	task_id = model.new_task(db, task_title, 1)
+	return redirect("/tasks")
 # Start our application server/web server/flask application when we run our program from the command line.
 # Start server in debug mode (to keep things simple)
 if __name__ == "__main__":
 		app.run(debug=True)
-
